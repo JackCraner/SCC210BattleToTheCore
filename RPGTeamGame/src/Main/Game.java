f5f9f43a8e15899937ee8ca8ba8645bd703ec863
Lifeless <j.garnett2@lancaster.ac.uk>
2020-11-29 17:18:26 +0000

    jack code that he cant commit

--- RPGTeamGame/src/Main/Game.java
+++ RPGTeamGame/src/Main/Game.java
@@ -1,8 +1,7 @@
 package Main;
 
-import Main.MapGen.ChunkLoader;
-import Main.MapGen.Map;
-import Main.Sprites.Player;
+import Main.Background.Background;
+import Main.Background.Sprites.Player;
 import org.jsfml.graphics.*;
 import org.jsfml.system.Clock;
 import org.jsfml.system.Vector2f;
@@ -14,21 +13,14 @@ import java.nio.file.Paths;
 
 public class Game
 {
-    ChunkLoader cH;
     Player playerObject;
     Texture playerTexture = new Texture();
     View playerView;
-    View miniMapView;
-    Text fpsCounter;
-    Font textFont = new Font();
     Clock systemClock = new Clock();
-    Map mapObject;
-
-    RenderWindow window;
+    Background bGround;
 
     int windowSize = 1000;
-    int viewSize = 1600;
-    int minimapViewSize = 4800;
+    int viewSize = 1600 * 3;
     int chunkSizeBlocks = 100;
     int chunkSizePixels = 1600;
 
@@ -42,17 +34,13 @@ public class Game
         try
         {
             playerTexture.loadFromFile(Paths.get("Assets\\Submarine.png"));
-            textFont.loadFromFile(Paths.get("Assets\\LEMONMILK-Regular.otf"));
         }
         catch(Exception E){}
 
-        fpsCounter = new Text("HI", textFont, 100);
-        playerView = new View(new Vector2f(viewSize/2,viewSize/2), new Vector2f(viewSize,viewSize));
-        miniMapView = new View(new Vector2f(viewSize/2,viewSize/2), new Vector2f(minimapViewSize,minimapViewSize));
-        playerObject = new Player(playerTexture, playerView,miniMapView,fpsCounter,3,new Vector2f(((numberOfChunksX/2) * chunkSizePixels) + (viewSize/2),viewSize/2));
-        mapObject = new Map(chunkSizeBlocks,chunkSizePixels,numberOfChunksX,numberOfChunksY);
-        cH = new ChunkLoader(chunkSizeBlocks,chunkSizePixels,mapObject,playerObject);
 
+        playerView = new View(new Vector2f(viewSize/2,viewSize/2), new Vector2f(viewSize,viewSize));
+        playerObject = new Player(playerTexture, playerView,3,new Vector2f(((numberOfChunksX/2) * chunkSizePixels) + (viewSize/2),viewSize/2));
+        bGround = new Background(chunkSizeBlocks, chunkSizePixels, numberOfChunksX, numberOfChunksY);
         runGame();
     }
 
@@ -65,12 +53,13 @@ public class Game
         RenderWindow window = new RenderWindow(new VideoMode(windowSize,windowSize),"Mine");
         //window.setFramerateLimit(60);
         int counter = 0;
+        bGround.initialiseBackGround(playerObject);
         while(window.isOpen())
         {
 
             if(systemClock.getElapsedTime().asSeconds() >= 1.f)
             {
-                fpsCounter.setString(String.valueOf(counter));
+                bGround.setFPS(counter);
                 counter = 0;
                 systemClock.restart();
             }
@@ -83,48 +72,33 @@ public class Game
             }
             if(Keyboard.isKeyPressed(Keyboard.Key.D))
             {
-                updateScene(1,0);
-
+                bGround.updateBackGroundOnMove(playerObject,1,0);
             }
             if(Keyboard.isKeyPressed(Keyboard.Key.A))
             {
-                updateScene(-1,0);
+                bGround.updateBackGroundOnMove(playerObject,-1,0);
 
             }
             if(Keyboard.isKeyPressed(Keyboard.Key.W))
             {
-                updateScene(0,-1);
+                bGround.updateBackGroundOnMove(playerObject,0,-1);
             }
             if(Keyboard.isKeyPressed(Keyboard.Key.S))
             {
-                updateScene(0,1);
+                bGround.updateBackGroundOnMove(playerObject,0,1);
 
             }
 
             window.clear();
             window.setView(playerView);
-            window.draw(cH);
-            window.draw(fpsCounter);
+            window.draw(bGround);
+
             window.draw(playerObject);
             window.display();
         }
     }
 
-    public void updateScene(int x, int y)
-    {
 
-        Vector2f currentPositionPlayer = playerObject.inChunk(chunkSizePixels);
-        playerObject.movePlayer(x,y);
-        if (currentPositionPlayer.x != playerObject.inChunk(chunkSizePixels).x || currentPositionPlayer.y != playerObject.inChunk(chunkSizePixels).y)
-        {
-            System.out.println(playerObject.inChunk(chunkSizePixels));
-            cH.generateMetaData();
-
-        }
-        cH.generateBlockArray();
-
-
-    }
 
 
 }
---
+++ RPGTeamGame/src/Main/Physics/Dynamic.java
@@ -0,0 +1,4 @@
+package Main.Physics;
+
+public interface Dynamic {
+}
