f7cba421a3d00ec0cd53eb98c6f1aa7bc13ab989
JackCraner <jackcraner12@gmail.com>
2020-12-07 12:54:51 +0000

    Add of Physics

---
+++ RPGTeamGame/src/Main/Physics/Collidable.java
@@ -0,0 +1,8 @@
+package Main.Physics;
+
+import org.jsfml.system.Vector2f;
+
+public interface Collidable {
+
+    public CollisionBox createCollisionBox();
+}
---
+++ RPGTeamGame/src/Main/Physics/MeshCreator.java
@@ -0,0 +1,10 @@
+package Main.Physics;
+
+public class MeshCreator
+{
+
+
+
+
+
+}
