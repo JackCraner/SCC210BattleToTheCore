d78ab94f3a7c8b6aecc67da6ad6845107d8e4f90
Ethan Chong <ethan.chong100@gmail.com>
2021-01-07 16:15:14 +0000

    New Update  - Changed all UI elements to singleton  - Added static variables for each element  - Added initialise code for each element

--- RPGTeamGame/src/Main/Game/GUI/GUIController.java
+++ RPGTeamGame/src/Main/Game/GUI/GUIController.java
@@ -3,28 +3,26 @@ package Main.Game.GUI;
 import Main.Game.Game;
 import org.jsfml.graphics.*;
 import org.jsfml.system.Vector2f;
-
 import java.io.File;
 import java.nio.file.Paths;
 
 public class GUIController implements Drawable
 {   
-
+    //All the UI elements are Singletons
     private static GUIController guicontroller = new GUIController();
+    private static HealthOrb HO = HealthOrb.getHealthOrb();             //Heath Orb
+    private static Inventory IB = Inventory.getInventory();             //Inventory
+    private static XPBar XPB = XPBar.getXPBar();                        //XP bar
+    private static MagicOrb MO = MagicOrb.getMagicOrb();                //Magic bar
+    private static MiniMap map = MiniMap.getMiniMap();                  //Mini Map
 
-    public static GUIController getGuicontroller() {
+    public static GUIController getGuicontroller(){
         return guicontroller;
     }
 
-
     Text fpsCounter;                //Displays the number of frames per second
     Font textFont = new Font();     //The font for Displaying text
-    HealthOrb hO;                   //Heath bar
-    Inventory iB;                   //Inventory
-    XPBar xpB;                      //XP bar
-    MagicOrb mO;
-    MiniMap map;                    //Mini Map
-    private GUIController()
+    public GUIController()
     {
     }
 
@@ -36,23 +34,23 @@ public class GUIController implements Drawable
     {
         try
         {
-            textFont.loadFromFile(Paths.get("Assets" + File.separator + "Fonts" + File.separator+ "LEMONMILK-Regular.otf"));
+            textFont.loadFromFile(Paths.get("Assets\\LEMONMILK-Regular.otf"));
         }
         catch (Exception e)
         {
             System.out.println("Font not found");
         }
-        hO = new HealthOrb();
-        mO = new MagicOrb();
-        iB = new Inventory(6);
-        xpB = new XPBar(iB.getTotalSlots());
-        map = new MiniMap();
-        hO.takeDamage(40);
-        xpB.AddXP(2);
-        hO.updateHealthOrbPosition();
-        mO.updateMagicOrbPosition();
-        iB.updateInventoryPosition();
-        xpB.updateXPBarPosition();
+        HO.InitialiseHealthOrb();
+        MO.InitialiseMagicOrb();
+        IB.InitialiseInventory(6);
+        XPB.InitialiseXPBar(IB.getTotalSlots());
+        map.InitialiseMiniMap();
+        HO.takeDamage(40);
+        XPB.AddXP(2);
+        HO.updateHealthOrbPosition();
+        MO.updateMagicOrbPosition();
+        IB.updateInventoryPosition();
+        XPB.updateXPBarPosition();
         map.updateMiniMapPosition();
         fpsCounter = new Text("HI", textFont, 100);     //Fps font and size
         fpsCounter.setPosition(new Vector2f(Game.getGame().getWindow().getSize().x-970,30));
@@ -64,10 +62,10 @@ public class GUIController implements Drawable
      */
     public void updateGUI()
     {
-        hO.updateHealthOrbPosition();
-        mO.updateMagicOrbPosition();
-        iB.updateInventoryPosition();
-        xpB.updateXPBarPosition();
+        HO.updateHealthOrbPosition();
+        MO.updateMagicOrbPosition();
+        IB.updateInventoryPosition();
+        XPB.updateXPBarPosition();
         map.updateMiniMapPosition();
         fpsCounter.setPosition(new Vector2f(Game.getGame().getWindow().getSize().x-970,30));
     }
@@ -91,20 +89,20 @@ public class GUIController implements Drawable
     @Override
     public void draw(RenderTarget renderTarget, RenderStates renderStates)
     {
-        renderTarget.draw(hO.getHealthOrbBack());
-        renderTarget.draw(hO.getHealthOrbFront());
+        renderTarget.draw(HO.getHealthOrbBack());
+        renderTarget.draw(HO.getHealthOrbFront());
 
-        renderTarget.draw(iB.getInventoryBack());
-        for (int i = 0; i < iB.getTotalSlots(); i++) {
-            renderTarget.draw(iB.getInventorySlots(i));
+        renderTarget.draw(IB.getInventoryBack());
+        for (int i = 0; i < IB.getTotalSlots(); i++) {
+            renderTarget.draw(IB.getInventorySlots(i));
         }
 
-        renderTarget.draw(xpB.getXPBarBack());
-        for (int i = 0; i < xpB.getTotalXPSlots(); i++) {
-            renderTarget.draw(xpB.getXPBarSlot(i));
+        renderTarget.draw(XPB.getXPBarBack());
+        for (int i = 0; i < XPB.getTotalXPSlots(); i++) {
+            renderTarget.draw(XPB.getXPBarSlot(i));
         }
-        renderTarget.draw(mO.getMagicOrbBack());
-        renderTarget.draw(mO.getMagicOrbFront());
+        renderTarget.draw(MO.getMagicOrbBack());
+        renderTarget.draw(MO.getMagicOrbFront());
         renderTarget.draw(map.getMiniMapFront());
         renderTarget.draw(map.getMiniMapBack());
         renderTarget.draw(fpsCounter);
